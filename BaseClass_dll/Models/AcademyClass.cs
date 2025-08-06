3a887893c390aeacc1e594fd11ddc37885d3bb5e
Shahanaj <fashionfuel267@gmail.com>
2025-08-06 13:38:24 +0600

    academicclass

--- BaseClass_dll/Models/AcademyClass.cs
+++ BaseClass_dll/Models/AcademyClass.cs
@@ -1,12 +1,16 @@
-﻿using static System.Collections.Specialized.BitVector32;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using static System.Collections.Specialized.BitVector32;
 
 namespace BaseClass_dll
 {
     public class AcademyClass : BaseEntity
     {
         public string Name { get; set; }
+        [ValidateNever]
         public ICollection<SchoolClass> SchoolClasses { get; set; } = new List<SchoolClass>();
+        [ValidateNever]
         public ICollection<Section> Sections { get; set; }
+        [ValidateNever]
         public ICollection<ClassSubject> ClassSubjects { get; set; }
     }
 }
